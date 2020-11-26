553e51342b25a8e75339b854abc649378805248f
vozgua <criotos@gmail.com>
2020-11-27 00:53:43 +0600

    Noise modifications via console

--- examples/noise/NoiseConsole.cpp
+++ examples/noise/NoiseConsole.cpp
@@ -19,7 +19,45 @@ bool NoiseConsole::handleCommand(const std::string& cmd)
 {
     if (cmd.substr(0, 7) == "octaves") 
     {
+        Poco::StringTokenizer t(cmd, " ", Poco::StringTokenizer::TOK_IGNORE_EMPTY | Poco::StringTokenizer::TOK_TRIM);
+        if (t.count() != 2)
+        {
+            _output = "octaves expects 1 arguments";
+            return true;
+        }
 
+        auto arg = t.begin();
+        arg++;
+        
+        int octaves = Poco::NumberParser::parse((*arg));
+        if (octaves != 1 && octaves != 8 && octaves != 64)
+        {
+            _output = "octaves must be 0..8..64";
+            return true;
+        }
+
+        _noise->clear();
+        _noise->params()->octaves = octaves;
+        return true;
+    }
+    else if (cmd.substr(0, 4) == "freq")
+    {
+        Poco::StringTokenizer t(cmd, " ", Poco::StringTokenizer::TOK_IGNORE_EMPTY | Poco::StringTokenizer::TOK_TRIM);
+
+        if (t.count() != 2)
+        {
+            _output = "freq expects 1 arguments";
+            return true;
+        }
+
+        auto arg = t.begin();
+        arg++;
+
+        float freq = Poco::NumberParser::parseFloat((*arg));
+        _noise->clear();
+        _noise->params()->freq = freq;
+        _noise->generate();
+        return true;
     }
     else if (cmd.substr(0, 4) == "seed")
     {
@@ -33,12 +71,12 @@ bool NoiseConsole::handleCommand(const std::string& cmd)
             return true;
         }
 
-        auto cmd = t.begin();
-        cmd++;
+        auto arg = t.begin();
+        arg++;
 
-        _engine->log()->info("New seed requested: {0}", (*cmd));
+        _engine->log()->info("New seed requested: {0}", (*arg));
 
-        uint32_t newSeed = Poco::NumberParser::parse((*cmd));
+        uint32_t newSeed = Poco::NumberParser::parse((*arg));
         
         _noise->clear();
         _noise->params()->seed = newSeed;
